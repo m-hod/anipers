911b5e382e24b4f36d26ab981d744bbf4fa3ac23
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-06-29 20:27:14 +1200

    update project name

--- android/app/src/main/java/com/anipers/MainActivity.java
+++ android/app/src/main/java/com/anipers/MainActivity.java
@@ -1,4 +1,4 @@
-package com.booruwallpapers;
+package com.anipers;
 
 import com.facebook.react.ReactActivity;
 
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "BooruWallpapers";
+    return "anipers";
   }
   
   
